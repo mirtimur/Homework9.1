97075fd1a4779bc2a97cda9961bf4e4f57289e89
Mir TImur <mirtimur1@gmail.com>
2023-12-22 12:21:33 +0300

    Initial commit

---
+++ TMSHomework9.1/TMSHomework9.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMSHomework9.1
+//
+//  Created by Timur Mir on 22.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
